2eb794ab2c6d95738be1fd3d9aad69a2cc85bee1
Ag <70638763+choeun01@users.noreply.github.com>
2024-01-07 22:16:13 +0900

    feat : 컴포넌트 만들기 -
    리액트에서 사용자 정의 태그는 컴포넌트라고 부르며, App.js 파일에 컴포넌트를 통하여 구현하였다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 컴포넌트의 첫 글자는 항상 대문자로 정의되어야 한다.
+function Header() {
+  return (
+    <header>
+      <h1><a href="/">React</a></h1>
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li><a href="/read/1">html</a></li>
+        <li><a href="/read/2">css</a></li>
+        <li><a href="/read/3">js</a></li>
+      </ol>
+    </nav>
+  );
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB
+    </article>
+  );
+}
+
+// 리액트에서는 사용자 정의 태그라는 말 대신 컴포넌트라는 말을 사용한다.
 function App() {
   return (
-    <div className="App">
-      Hello React!
+    <div>
+      {/* 컴포넌트를 사용할 때에는 태그 형태로 사용한다. */}
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
