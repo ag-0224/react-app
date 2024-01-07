72f43fe5305b294a6f9b48aee30667df43b75581
Ag <70638763+choeun01@users.noreply.github.com>
2024-01-08 01:06:54 +0900

    feat : create -
    state 속성을 활용하여 create 기능을 추가하였다.

--- src/App.js
+++ src/App.js
@@ -49,6 +49,24 @@ function Article(props) {
   );
 }
 
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form onSubmit={event=>{
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onCreate(title, body);
+      }}>
+        <p><input type="text" name="title" placeholder="title"/></p>
+        <p><textarea name="body" placeholder="body"></textarea></p>
+        <p><input type="submit" value="Create"></input></p>
+      </form>
+    </article>
+  );
+}
+
 // 리액트에서는 사용자 정의 태그라는 말 대신 컴포넌트라는 말을 사용한다.
 function App() {
   // const _mode = useState('WELCOME');
@@ -58,11 +76,12 @@ function App() {
   // 위 3줄의 코드와 아래 축약형 코드는 서로 같다. 주로 축약형을 많이 사용한다.
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'},
-  ]
+  ]);
   let content = null;
   if (mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, WEB"></Article>;
@@ -74,7 +93,19 @@ function App() {
         body = topics[i].body;
       }
     }
-    content =  <Article title={title} body={body}></Article>
+    content =  <Article title={title} body={body}></Article>;
+  } else if (mode === 'CREATE') {
+    content = (
+      <Create onCreate={(_title, _body)=>{
+        const newTopic = {id: nextId, title:_title, body:_body}
+        const newTopics = [...topics]
+        newTopics.push(newTopic);
+        setTopics(newTopics);
+        setMode('READ');
+        setId(nextId);
+        setNextId(nextId+1);
+    }}></Create>
+    );
   }
   return (
     <div>
@@ -89,6 +120,10 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
+      <a href="/create" onClick={(event)=>{
+        event.preventDefault();
+        setMode('CREATE');
+      }}>create</a>
     </div>
   );
 }
